593b5195ac63fbc13268675d68956cfa78f9f186
Swayam gandhi <swayamgandhi6@gmail.com>
2026-08-24 16:14:52 +0530

    updates

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -494,7 +494,7 @@ export interface ApiBuffetCategoryBuffetCategory
     > &
       Schema.Attribute.Private;
     order: Schema.Attribute.Integer;
-    price: Schema.Attribute.Integer;
+    price: Schema.Attribute.Decimal;
     publishedAt: Schema.Attribute.DateTime;
     title: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
